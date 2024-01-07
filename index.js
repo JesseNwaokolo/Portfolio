90bc498bbeebbba63720f638be2babd5e8780cfd
JesseNwaokolo <109309010+JesseNwaokolo@users.noreply.github.com>
2024-01-07 13:29:33 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function pop(event){
+    alert("services aren't limited, you can contact me from the home page")
+}
+
+function m(){
+    document.querySelector(".ham-menu").style.visibility = "visible"
+}
+
+function x(){
+    document.querySelector(".ham-menu").style.visibility = "hidden"
+}
